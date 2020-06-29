bfefba59dbcd65e7ad5564d050eb3db2533d2534
BattlefieldDuck <hkdsethomas@gmail.com>
2020-06-29 12:10:35 +0800

    Create music upload function

--- app/src/main/java/com/example/musiclips/firebase/MusicStorage.kt
+++ app/src/main/java/com/example/musiclips/firebase/MusicStorage.kt
@@ -1,26 +1,26 @@
 package com.example.musiclips.firebase
 
 import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.UploadTask
 import java.io.File
 import java.io.FileInputStream
 
-class Storage {
+class MusicStorage {
 
-    fun uploadMusic(userId: String, fileName: String) {
+    fun uploadMusic(userId: String, fileName: String, filePath: String): UploadTask {
         val storageRef = FirebaseStorage.getInstance().reference;
-        val fileRef = storageRef.child("mountains.jpg")
-        val mountainImagesRef = storageRef.child("images/mountains.jpg")
+        val fileRef = storageRef.child(fileName)
+        val mountainImagesRef = storageRef.child("${userId}/${fileName}")
 
 // While the file names are the same, the references point to different files
         //mountainsRef.name == mountainImagesRef.name // true
         //mountainsRef.path == mountainImagesRef.path // false
+        val stream = FileInputStream(File(filePath))
 
-
-        val stream = FileInputStream(File("path/to/images/rivers.jpg"))
-
-        var uploadTask = fileRef.putStream(stream)
-        uploadTask.addOnFailureListener {
+        return fileRef.putStream(stream);
+        fileRef.putStream(stream).addOnFailureListener {
             // Handle unsuccessful uploads
+
         }.addOnSuccessListener {
             // taskSnapshot.metadata contains file metadata such as size, content-type, etc.
             // ...
