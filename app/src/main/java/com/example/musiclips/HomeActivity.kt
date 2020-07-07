c04b0b86180d5576fbc949ffc71e6429ff6d2549
humanity <orkevin947@gmail.com>
2020-07-07 11:00:45 +0800

    update menu header add upload icon

--- app/src/main/java/com/example/musiclips/HomeActivity.kt
+++ app/src/main/java/com/example/musiclips/HomeActivity.kt
@@ -1,5 +1,6 @@
 package com.example.musiclips
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
@@ -9,8 +10,12 @@ import androidx.fragment.app.FragmentTransaction
 import com.example.musiclips.fragments.HomeFragment
 import com.example.musiclips.fragments.MyAlbumsFragment
 import com.example.musiclips.fragments.MySongsFragment
+import com.example.musiclips.tools.getFileName
+import com.example.musiclips.tools.uploadMusicToFirebaseStorage
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_home.*
+import java.io.File
+import java.io.FileInputStream
 
 
 class HomeActivity : AppCompatActivity() {
@@ -18,6 +23,8 @@ class HomeActivity : AppCompatActivity() {
     private lateinit var homeFragment: HomeFragment
     private lateinit var myAlbumsFragment: MyAlbumsFragment
     private lateinit var mySongsFragment: MySongsFragment
+
+    val UPLOAD_MUSIC_REQUESTCODE : Int = 10
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,6 +36,13 @@ class HomeActivity : AppCompatActivity() {
         myAlbumsFragment = MyAlbumsFragment.newInstance("", "")
         mySongsFragment = MySongsFragment.newInstance("", "")
         setUpFragment(homeFragment)
+
+        btn_upload.setOnClickListener {
+            val intent = Intent()
+            intent.action = Intent.ACTION_GET_CONTENT
+            intent.type = "audio/*"
+            startActivityForResult(intent, UPLOAD_MUSIC_REQUESTCODE)
+        }
 
         button_Settings.setOnClickListener {
             startActivity(Intent(this, SettingsActivity::class.java))
@@ -55,5 +69,28 @@ class HomeActivity : AppCompatActivity() {
             .addToBackStack(fragment.toString())
             .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
             .commit()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        when (requestCode) {
+            UPLOAD_MUSIC_REQUESTCODE -> {
+                if (resultCode == Activity.RESULT_OK) {
+                    val audioUri = data!!.data!!
+                    val fileInputStream = if (audioUri.scheme.equals("content")) {
+                        contentResolver.openInputStream(audioUri)!!
+                    } else {
+                        FileInputStream(File(audioUri.path!!))
+                    }
+
+                    uploadMusicToFirebaseStorage(
+                        auth.currentUser!!,
+                        getFileName(contentResolver, audioUri),
+                        fileInputStream
+                    )
+                }
+            }
+        }
     }
 }
